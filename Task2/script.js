7a01b2edceb91ee8d8d7dd5c6e095162accd21b3
riasa0895 <143418298+riasa0895@users.noreply.github.com>
2026-03-11 19:51:02 +0100

    Add function to fetch and display user data

--- Task2/script.js
+++ Task2/script.js
@@ -1,1 +1,14 @@
+function getUser() {
 
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    document.getElementById("result").textContent =
+      JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+
+}
