9a4e19582cb7365bc046fcc46de732c6e0ff0b13
riasa0895 <143418298+riasa0895@users.noreply.github.com>
2026-03-11 19:44:45 +0100

    Add student object with personal details

--- Task1/script.js
+++ Task1/script.js
@@ -1,1 +1,24 @@
+let student = {
+  studentId: "S-123456",
+  name: {
+    first: "John",
+    last: "Doe",
+    preferred: "Joe"
+  },
+  age: 21,
+  contact: {
+    email: "john.doe@example.edu",
+    phone: {
+      mobile: "+47 234 45 678",
+      home: "(not provided)"
+    },
+    address: {
+      street: "Pilestredet 46",
+      city: "Oslo",
+      postalCode: "0167",
+      country: "Norway"
+    }
+  }
+};
 
+console.log(student);
